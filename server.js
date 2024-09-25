b595cf16ead74480e5117a96eec38751786aca6e
EugeneBoyah <boyaheugene1999@gmail>
2024-09-25 23:44:01 +0000

    Revert "completed the league pages"
    This reverts commit 8690bfb6878f92b73043ac899dee56d213b4aa50.

--- server.js
+++ server.js
@@ -404,7 +404,7 @@ app.get("/league_overview/:league_id", (req, res) => {
             }
 
         // Render the league overview page with the fetched league, teams, and matches data
-        res.render("league_overview", { leagueId, league, teams, matches, countymatches, leagueStand,  title: `LibScore |${league.league_name}` });
+        res.render("league_overview", { leagueId, league, teams, matches, countymatches,  title: `LibScore |${league.league_name}` });
       });
     });
     });
